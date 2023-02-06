89404e10fa98533e556a2a444bab6198e178f0e7
文莉李 <wenlili@wenlilideMacBook-Pro.local>
2023-02-06 10:04:58 -0800

    Compelte hw4

--- assignment4/hw4.c
+++ assignment4/hw4.c
@@ -1,5 +1,5 @@
-// name: <your name here>
-// email: <your email here>
+// name: <Wenli Li>
+// email: <li.wenli@northeastern.edu>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,6 +19,7 @@ int main(){
   int i;             // loop variable
   int j;             // loop variable
   int smallest;      // current smallest element
+  int k=0;
 
   //seed random numbers
   srand((unsigned)time(NULL));
@@ -46,20 +47,40 @@ int main(){
   printf("\n");
 
   //selection sort
-  for (i=0; i<LIMIT; i++) {
+  smallest = 0;
+  int a = 0;
 
-    // INSERT YOUR CODE HERE
-    
-  }
+  for (i=0; i<LIMIT; i++) {
+    //reset smallest to a valid value.
+    while(a<LIMIT && !valid[a]) {
+      a++;
+    }
+    smallest = a;
+    // Find the smallest number in the valid array.
+    for (j=0; j<LIMIT; j++) {
+      if (valid[j] == true && source[j] <= source[smallest]) {
+        smallest = j;
+        }
+      }
+
+    valid[smallest] = false;
+    dest[i] = source[smallest];
+    }
+    // INSERT YOUR CODE HERE 
+  
   
   //print out sorted array in rows of 10
   printf("Destination array:\n");
-  
+  for (i=0; i < ((LIMIT/10)+1); i++) {
+    for (j=0; j<10; j++) {
+      if (i*10+j < LIMIT) {
+	printf("%.2d ", dest[i*10+j]);
+      }
+    }
+    printf("\n");
+  }
 
   // INSERT YOUR CODE HERE
-    
- 
-  
-  
+
   return 0;
 }
