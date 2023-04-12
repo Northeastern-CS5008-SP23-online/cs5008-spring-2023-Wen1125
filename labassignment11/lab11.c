2c59299d4ed81cb3296e08864e2cf4f13e096011
文莉李 <wenlili@wenlilideMBP.askey.com>
2023-04-12 11:29:33 -0700

    update lab11

--- labassignment11/lab11.c
+++ labassignment11/lab11.c
@@ -15,12 +15,12 @@ void *philosopher(void *x)
        /*-----Insert your code here----*/
       printf("Philosopher %d is thinking.\n", n+1);
     	pthread_mutex_lock(&chopstick[n]);
-    	pthread_mutex_lock(&chopstick[n+1])%5; // this needs to be a pointer - address 
+    	pthread_mutex_lock(&chopstick[(n+1)%5]);
 	    printf("Philosopher %d is eating using chopstick[%d] and chopstick[%d].\n", n+1, n, (n+1)%5);
 	    sleep(1);
 	    printf("Philosopher %d finished eating.\n", n+1);
     	pthread_mutex_unlock(&chopstick[n]);
-    	pthread_mutex_unlock(&chopstick[n+1])%5;
+    	pthread_mutex_unlock(&chopstick[(n+1)%5]);
     	return NULL; 
 }
 
